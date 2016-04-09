6bef4e1f80b7dc30327c28b06a529df4981e4925
AnshulGupta96 <gupanshul96@gmail.com>
2016-04-09 10:21:09 +0530

    Brainex - prodex event
    I made the front end of the home page for society prodex for its event brainex using html and css.

---
+++ index.php
@@ -0,0 +1,104 @@
+<!doctype html>
+<html lang="eng">
+<head>
+<title>Welcome to prodex Brainstorming page.</title>
+<link rel="shortcut icon" href="logoprodex.png" />
+<link rel="stylesheet" type="text/css" href="index.css">
+<script type="text/javascript"src="jquery.js"></script>
+<script type="text/javascript"src="index.js"></script>
+</head>
+<body>
+<div id="whole">
+<div id="header">
+	<p id="logo">
+		<img src="image/Brainex_prerit1.png" id="logo_brain" alt="Logo Brain">
+	</p>
+</div>
+<table>
+<tr>
+<td>
+<div id="container_left">
+	<div class="link" id="link1">
+		<img src="image/event.png" alt="bulb" id="img_bulb">
+		<span id="title_right" class="span_events"> EVENTS </span>
+	</div>
+	<div class="link" id="link2">
+		<img src="image/calender.png" alt="timeline" id="img_timeline">
+		<span id="title_right_right" class="span_timeline"> Timeline </span>
+	</div>
+	<div class="link" id="link3">
+		<img src="image/rule1.png" alt="rule" id="img_rule">
+		<span id="title_right" class="span_rule"> Rules </span>
+	</div>
+	<div class="link" id="link4">
+		<img src="image/contact.png" alt="contact" id="img_contact">
+		<span id="title_right_right" class="span_contact"> Contact us </span>
+	</div>
+</div>
+</td>
+<td>
+<div id="container_center">
+	<div id="center_top">
+		<table>
+			<tr>
+				<td rowspan="2"><img src="image/brainex_logo.png" id="logo_brainex" alt="Brainex Logo"></td>
+				<td id="powered">Powered By</td>
+				<td rowspan="2"><img src="image/pd (1).png" id="logo_pd" alt="Prodex Logo"></td>
+			</tr>
+			<tr>
+				<td><img src="image/prodexh_1_.png" id="img_pd" alt="Prodex"></td>
+			</tr>
+			<tr>
+				<td colspan="3" id="gearup">Gear Up to Challenge <br/> Your Brain!</td>
+			</tr>
+		</table>
+	</div>
+	<div id="center_bottom">
+	<img src="gears/gear25.png" class="gear01">	
+	<img src="gears/gear26.png" class="gear26">
+	<img src="gears/gear2.png" class="gear02">
+	<img src="gears/gear3.png" class="gear012">
+	<img src="gears/gear5.png" class="gear011">
+	<img src="gears/gear16.png" class="gear08">
+	<img src="gears/gear6.png" class="gear04">
+	<img src="gears/gear8.png" class="gear09">
+	<img src="gears/gear17.png" class="gear010">
+	<img src="gears/gear9.png" class="gear07">
+	<img src="gears/gear10.png" class="gear03">
+	<img src="gears/gear11.png" class="gear06">
+	<img src="gears/gear20.png" class="gear20">
+	<img src="gears/gear21.png" class="gear21">
+	<img src="gears/gear30.png" class="gear30">
+	<img src="gears/gear31.png" class="gear31">
+	<img src="gears/gear33.png" class="gear33">
+	<img src="gears/gear35.png" class="gear35">
+	
+	</div>
+</div>
+</td>
+<td>
+<div id="container_right">
+	<div id="rigt_top">
+		<button id="register" type="button" onclick="">Register</button>
+		<br>
+		<br>
+		<button id="login" type="button" onclick="">LogIn</button>
+	</div>
+	<div id="right_bottom">
+		<div id="sponsor">
+			<img src="image/sponsor.png" id="img_sponsor spons1" alt="Sponsor Image">
+			
+		</div>
+		<div id="last_gear">
+			<img src="gears/gear36.png" alt="Last Gear" id="img_lastgear">
+		</div>
+		
+	</div>
+</div>
+</td>
+</tr>
+</table>
+</div>
+</body>
+
+</html>
